5654a048e9bcbeab5da01c0f17ecc252352f8333
Emykhet <49447920+Emykhet@users.noreply.github.com>
2020-11-23 23:00:55 +0100

    reducers folder deleted

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux"
-
-import {fetchSongReducer} from "./songReducers"
-
-const allReducers = combineReducers({
-    songList: fetchSongReducer,
-})
-
-export default allReducers
